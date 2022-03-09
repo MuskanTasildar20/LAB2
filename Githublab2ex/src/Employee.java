166f4c7bd1b53e89e51c37aac42264e23d8ab700
admin <admin@Bcalab4-m17>
2022-03-09 10:36:28 +0530

    Committing the project

--- Githublab2ex/src/Employee.java
+++ Githublab2ex/src/Employee.java
@@ -1,13 +1,13 @@
 
-public class Student {
+public class Employee {
 	String name;
-	Student(String name){
+	Employee(String name){
 		this.name=name;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Student details");
+		System.out.println("Emoployee name");
 
 	}
 
