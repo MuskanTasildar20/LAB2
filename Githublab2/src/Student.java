2b8603d0e2c75adf6cff6c2fa3e50fcd2a71ede6
admin <admin@Bcalab4-m17>
2022-03-16 10:52:00 +0530

    commit code

--- Githublab2/src/Student.java
+++ Githublab2/src/Student.java
@@ -4,6 +4,9 @@ public class Student {
 	Student(String name){
 		this.name=name;
 	}
+	void dispaly() {
+		System.out.println("name");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
